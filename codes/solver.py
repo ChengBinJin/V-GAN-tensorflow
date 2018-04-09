964b0c381cf1978eb1ac923cbb6791b1259325a3
chengbin <sbkim0407@gmail.com>
2018-04-09 13:30:10 +0900

    check solver.py

--- codes/solver.py
+++ codes/solver.py
@@ -38,11 +38,6 @@ class Solver(object):
         tf_utils.show_all_variables()
 
     def _make_folders(self):
-        self.model_out_dir = "{}/model_{}_{}_{}".format(self.flags.dataset, self.flags.discriminator,
-                                                        self.flags.train_interval, self.flags.batch_size)
-        if not os.path.isdir(self.model_out_dir):
-            os.makedirs(self.model_out_dir)
-
         if self.flags.is_test:
             self.img_out_dir = "{}/seg_result_{}_{}_{}".format(self.flags.dataset,
                                                                self.flags.discriminator,
@@ -57,8 +52,12 @@ class Solver(object):
                 os.makedirs(self.auc_out_dir)
 
         elif not self.flags.is_test:
+            self.model_out_dir = "{}/model_{}_{}_{}".format(self.flags.dataset, self.flags.discriminator,
+                                                            self.flags.train_interval, self.flags.batch_size)
             self.sample_out_dir = "{}/sample_{}_{}_{}".format(self.flags.dataset, self.flags.discriminator,
                                                               self.flags.train_interval, self.flags.batch_size)
+            if not os.path.isdir(self.model_out_dir):
+                os.makedirs(self.model_out_dir)
             if not os.path.isdir(self.sample_out_dir):
                 os.makedirs(self.sample_out_dir)
 
@@ -87,26 +86,30 @@ class Solver(object):
     def test(self):
         if self.load_model():
             print(' [*] Load Success!\n')
+            self.eval(phase='test')
         else:
             print(' [!] Load Failed!\n')
-
-        self.eval(phase='test')
 
     def sample(self, iter_time):
         if np.mod(iter_time, self.flags.sample_freq) == 0:
             idx = np.random.choice(self.dataset.num_val, 2, replace=False)
             x_imgs, y_imgs = self.dataset.val_imgs[idx], self.dataset.val_vessels[idx]
             samples = self.model.sample_imgs(x_imgs)
+
             # masking
             seg_samples = utils.remain_in_mask(samples, self.dataset.val_masks[idx])
+
             # crop to original image shape
             x_imgs_ = utils.crop_to_original(x_imgs, self.dataset.ori_shape)
             seg_samples_ = utils.crop_to_original(seg_samples, self.dataset.ori_shape)
             y_imgs_ = utils.crop_to_original(y_imgs, self.dataset.ori_shape)
+
+            # sampling
             self.plot(x_imgs_, seg_samples_, y_imgs_, iter_time, idx=idx, save_file=self.sample_out_dir,
                       phase='train')
 
     def plot(self, x_imgs, samples, y_imgs, iter_time, idx=None, save_file=None, phase='train'):
+        # initialize grid size
         cell_size_h, cell_size_w = self.dataset.ori_shape[0] / 100, self.dataset.ori_shape[1] / 100
         num_columns, margin = 3, 0.05
         width = cell_size_w * num_columns
@@ -128,14 +131,14 @@ class Solver(object):
             x_imgs_norm[_] = np.expand_dims(x_imgs[_], axis=0) * std + mean
         x_imgs_norm = x_imgs_norm.astype(np.uint8)
 
-        # gray scale to color scale
+        # 1 channel to 3 channels
         samples_3 = np.stack((samples, samples, samples), axis=3)
-        y_imgs = np.stack((y_imgs, y_imgs, y_imgs), axis=3)
+        y_imgs_3 = np.stack((y_imgs, y_imgs, y_imgs), axis=3)
 
-        imgs = [x_imgs_norm, samples_3, y_imgs]
+        imgs = [x_imgs_norm, samples_3, y_imgs_3]
         for col_index in range(len(imgs)):
             for row_index in range(x_imgs.shape[0]):
-                ax = plt.subplot(gs[row_index * 3 + col_index])
+                ax = plt.subplot(gs[row_index * num_columns + col_index])
                 plt.axis('off')
                 ax.set_xticklabels([])
                 ax.set_yticklabels([])
@@ -151,7 +154,8 @@ class Solver(object):
             plt.savefig(os.path.join(save_file, 'compared_{}.png'.format(os.path.basename(
                 self.dataset.test_img_files[idx[0]])[:-4])), bbox_inches='tight')
             plt.close(fig)
-            # save
+
+            # save vessel alone, vessel should be uint8 type
             Image.fromarray(np.squeeze(samples*255).astype(np.uint8)).save(os.path.join(
                 save_file, '{}.png'.format(os.path.basename(self.dataset.test_img_files[idx[0]][:-4]))))
 
@@ -183,9 +187,10 @@ class Solver(object):
                 x_img = imgs[iter_]
                 x_img = np.expand_dims(x_img, axis=0)  # (H, W, C) to (1, H, W, C)
 
+                # measure inference time
                 start_time = time.time()
                 generated_vessel = self.model.sample_imgs(x_img)
-                total_time += time.time() - start_time
+                total_time += (time.time() - start_time)
 
                 generated.append(np.squeeze(generated_vessel, axis=(0, 3)))  # (1, H, W, 1) to (H, W)
 
@@ -196,10 +201,12 @@ class Solver(object):
             if phase == 'test':
                 # save test images
                 segmented_vessel = utils.remain_in_mask(generated, masks)
+
                 # crop to original image shape
                 imgs_ = utils.crop_to_original(imgs, self.dataset.ori_shape)
                 cropped_vessel = utils.crop_to_original(segmented_vessel, self.dataset.ori_shape)
                 vessels_ = utils.crop_to_original(vessels, self.dataset.ori_shape)
+
                 for idx in range(num_data):
                     self.plot(np.expand_dims(imgs_[idx], axis=0),
                               np.expand_dims(cropped_vessel[idx], axis=0),
@@ -209,14 +216,18 @@ class Solver(object):
         return dic_coeff
 
     def measure(self, generated, vessels, masks, num_data, iter_time, phase, total_time):
+        # masking
         vessels_in_mask, generated_in_mask = utils.pixel_values_in_mask(
             vessels, generated, masks)
+
+        # averaging processing time
         avg_pt = (total_time / num_data) * 1000  # average processing tiem
 
-        # evaluation
+        # evaluate Area Under the Curve of ROC and Precision-Recall
         auc_roc = utils.AUC_ROC(vessels_in_mask, generated_in_mask)
         auc_pr = utils.AUC_PR(vessels_in_mask, generated_in_mask)
 
+        # binarize to calculate Dice Coeffient
         binarys_in_mask = utils.threshold_by_otsu(generated, masks)
         dice_coeff = utils.dice_coefficient_in_train(vessels_in_mask, binarys_in_mask)
         acc, sensitivity, specificity = utils.misc_measures(vessels_in_mask, binarys_in_mask)
@@ -228,15 +239,13 @@ class Solver(object):
                                               ('sensitivity', sensitivity), ('specificity', specificity),
                                               ('score', score), ('best_dice_coeff', self.best_dice_coeff),
                                               ('avg_pt', avg_pt)])
-
         utils.print_metrics(iter_time, ord_output)
 
-        # write in tensorboard
+        # write in tensorboard when in train mode only
         if phase == 'train':
             self.model.measure_assign(
                 auc_pr, auc_roc, dice_coeff, acc, sensitivity, specificity, score, iter_time)
-
-        if phase == 'test':
+        elif phase == 'test':
             # write in npy format for evaluation
             utils.save_obj(vessels_in_mask, generated_in_mask,
                            os.path.join(self.auc_out_dir, "auc_roc.npy"),
